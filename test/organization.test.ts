85d67687e1dfa028ab781003f07de2939355ada9
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2023-07-01 08:18:00 +0100

    Move a test file

---
+++ test/organization.test.ts
@@ -0,0 +1,29 @@
+import { beforeEach, expect, describe, it } from 'vitest';
+import { HCB_Organization } from '../src/api_endpoints/organization';
+
+
+describe('HCB_Organization', () => {
+  let organization: HCB_Organization;
+  beforeEach(() => {
+    organization = new HCB_Organization();
+  });
+
+  describe('singleOrganization', () => {
+    it('hq EQUALS org_a29uVj', async () => {
+      const data = { id: 'hq' };
+      const result = await organization.singleOrganization(data);
+      expect(result).toBeDefined();
+      expect(result.id).toEqual('org_a29uVj');
+      expect(result.object).toEqual('organization');
+    });
+  });
+  describe('allOrganizations', async () => {
+    it('allOrganizations object check on first 2 items', async () => {
+      const result = await organization.allOrganizations({per_page: 2});
+      expect(result).toBeDefined();
+      result.forEach((org: any) => {
+        expect(org.object).toEqual('organization');
+      });
+    });
+  });
+});
