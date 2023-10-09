4a0d3eae5013bb0129103e94da93bbfd5b8cbcbd
Sean Outram <outramsean0@gmail.com>
2023-10-09 12:27:44 +0000

    refactor: fixed some wrong URL's

--- src/enums/hcb_logos.ts
+++ src/enums/hcb_logos.ts
@@ -4,12 +4,12 @@ export enum ENUM_HCB_LOGO_SVG {
 	 * @description HCB Logo enum for the SVG form
 	 * @enum ENUM_HCB_LOGO_SVG
 	 */
-	icon_original = 'https://bank.hackclub.com/brand/hcb-icon-icon-original.svg',
-	icon_dark = 'https://bank.hackclub.com/brand/hcb-icon-icon-dark.svg',
-	badge_original_light = 'https://bank.hackclub.com/brand/hcb-logo-original-light.svg',
-	badge_original_dark = 'https://bank.hackclub.com/brand/hcb-logo-original-dark.svg',
-	badge_outlined_black = 'https://bank.hackclub.com/brand/hcb-logo-outlined-black.svg',
-	badge_outlined_white = 'https://bank.hackclub.com/brand/hcb-logo-outlined-white.svg'
+	icon_original = 'https://hcb.hackclub.com/brand/hcb-icon-icon-original.svg',
+	icon_dark = 'https://hcb.hackclub.com/brand/hcb-icon-icon-dark.svg',
+	badge_original_light = 'https://hcb.hackclub.com/brand/hcb-logo-original-light.svg',
+	badge_original_dark = 'https://hcb.hackclub.com/brand/hcb-logo-original-dark.svg',
+	badge_outlined_black = 'https://hcb.hackclub.com/brand/hcb-logo-outlined-black.svg',
+	badge_outlined_white = 'https://hcb.hackclub.com/brand/hcb-logo-outlined-white.svg'
 }
 
 // PNG
@@ -18,10 +18,10 @@ export enum ENUM_HCB_LOGO_PNG {
 	 * @description HCB Logo enum for the PNG form
 	 * @enum ENUM_HCB_LOGO_PNG
 	 */
-	icon_original = 'https://bank.hackclub.com/brand/hcb-icon-icon-original.png',
-	icon_dark = 'https://bank.hackclub.com/brand/hcb-icon-icon-dark.png',
-	badge_original_light = 'https://bank.hackclub.com/brand/hcb-logo-original-light.png',
-	badge_original_dark = 'https://bank.hackclub.com/brand/hcb-logo-original-dark.png',
-	badge_outlined_black = 'https://bank.hackclub.com/brand/hcb-logo-outlined-black.png',
-	badge_outlined_white = 'https://bank.hackclub.com/brand/hcb-logo-outlined-white.png'
+	icon_original = 'https://hcb.hackclub.com/brand/hcb-icon-icon-original.png',
+	icon_dark = 'https://hcb.hackclub.com/brand/hcb-icon-icon-dark.png',
+	badge_original_light = 'https://hcb.hackclub.com/brand/hcb-logo-original-light.png',
+	badge_original_dark = 'https://hcb.hackclub.com/brand/hcb-logo-original-dark.png',
+	badge_outlined_black = 'https://hcb.hackclub.com/brand/hcb-logo-outlined-black.png',
+	badge_outlined_white = 'https://hcb.hackclub.com/brand/hcb-logo-outlined-white.png'
 }
